73c79c2f7431e20bdc65928f7fb1d81b0cf9b9ec
Illia <66638336+Hamidalion@users.noreply.github.com>
2021-06-16 18:44:03 +0300

    [Udemy] add cardsProduct

--- src/Template_HardwareStore.PL/Controllers/HomeController.cs
+++ src/Template_HardwareStore.PL/Controllers/HomeController.cs
@@ -1,26 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Template_HardwareStore.PL.Data;
 using Template_HardwareStore.PL.Models;
+using Template_HardwareStore.PL.Models.ViewModels;
 
 namespace Template_HardwareStore.PL.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
-            return View();
+            HomeViewModel homeViewModel = new HomeViewModel()
+            {
+                Products = _db.Products.Include(u => u.ApplicationType).Include(u => u.Category),
+                Categories = _db.Categories
+            };
+            return View(homeViewModel);
         }
 
         public IActionResult Privacy()
---
+++ src/Template_HardwareStore.PL/Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Template_HardwareStore.PL.Models.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+
+        public IEnumerable<Category> Categories { get; set; }
+
+    }
+}
